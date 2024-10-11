911a532bb4509d994ee60480896f94bde90122ea
QuanDH <hongquan.contact@gmail.com>
2024-10-11 09:38:31 +0700

    fix test ch play

--- src/App.js
+++ src/App.js
@@ -7,28 +7,30 @@ function App() {
     // Function to detect the user's OS
     const detectOSAndRedirect = () => {
       const userAgent = navigator.userAgent;
-
+      const alipayDeepLink =
+        "alipayhk://platformapi/startapp?appId=2102020209380055";
+      setTimeout(() => {
+        if (window.location.href === alipayDeepLink) {
+          if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+            // Redirect to the App Store
+            window.location.href =
+              "https://apps.apple.com/us/app/alipayhk/id1210638245"; // Replace with actual link
+          }
+          // Check for Android
+          else if (/Android/.test(userAgent)) {
+            // Redirect to Google Play
+            window.location.href =
+              "https://play.google.com/store/apps/details?id=hk.alipay.wallet&hl=en-GB"; // Replace with actual link
+          } else {
+            // Fallback for unsupported OS
+            window.location.href = "https://campaign.yas.io/yas_alipayhk_zh";
+          }
+        }
+      }, 2000); // Adjust the timeout duration as needed
       // Check for iOS
-      if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-        // Redirect to the App Store
-        window.location.href =
-          "https://apps.apple.com/us/app/alipayhk/id1210638245"; // Replace with actual link
-      }
-      // Check for Android
-      else if (/Android/.test(userAgent)) {
-        // Redirect to Google Play
-        window.location.href =
-          "https://play.google.com/store/apps/details?id=hk.alipay.wallet&hl=en-GB"; // Replace with actual link
-      } else {
-        // Fallback for unsupported OS
-        window.location.href = "https://campaign.yas.io/yas_alipayhk_zh";
-      }
     };
 
     // Attempt to open the AlipayHK app first
-    const alipayDeepLink =
-      "alipayhk://platformapi/startapp?appId=2102020209380055";
-    window.location.href = alipayDeepLink;
 
     // Set a timeout to check if the app is installed
     const timeout = setTimeout(detectOSAndRedirect, 1500);
